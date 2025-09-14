4b02f752bfb157de02edce8e550e413f6c2da66c
Alexander Large <alexanderlarge@Alexanders-Mac-mini.local>
2025-09-14 14:02:43 +0100

    vault backup: 2025-09-14 14:02:43

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://www.alexislearning.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
